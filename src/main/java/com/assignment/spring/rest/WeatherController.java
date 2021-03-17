3cdc204ad3680fb5ffc12451d0be143b4e43510a
Sorel Mitra <>
2021-03-17 11:47:59 +0200

    Change request to a POST and add city parameter.

--- src/main/java/com/assignment/spring/rest/WeatherController.java
+++ src/main/java/com/assignment/spring/rest/WeatherController.java
@@ -9,7 +9,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -26,12 +28,11 @@ public class WeatherController {
     @Autowired
     private WeatherRepository weatherRepository;
 
-    @RequestMapping("/weather")
-    public ResponseEntity<WeatherRestResponse> weather(HttpServletRequest request) {
+    @PostMapping("/weather")
+    public ResponseEntity<WeatherRestResponse> weather(@RequestParam String city) {
         String methodName = "weather";
 
-        String city = request.getParameter("city");
-        LOG.info("{}: path {}, city {}, query {}", methodName, request.getServletPath(), city, request.getQueryString());
+        LOG.info("{}: city <{}>", methodName, city);
         if (city == null) {
             return buildFailureResponse();
         }
