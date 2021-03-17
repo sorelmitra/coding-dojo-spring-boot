69021d0fc741f252be9041616d768d20dc2f4e88
Sorel Mitra <>
2021-03-17 13:12:20 +0200

    REST response for Weather.

---
+++ src/main/java/com/assignment/spring/rest/WeatherRestResponse.java
@@ -0,0 +1,58 @@
+package com.assignment.spring.rest;
+
+public class WeatherRestResponse {
+    private String id;
+    private String city;
+    private String country;
+    private Double temperature;
+    private boolean success;
+    private String reason;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public Double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(Double temperature) {
+        this.temperature = temperature;
+    }
+
+    public boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+}
