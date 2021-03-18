3b1c5cbb45ae03fd85bdbf76a355321ff7590fda
Sorel Mitra <>
2021-03-18 16:35:57 +0200

    Add stress testing using K6.

---
+++ src/test/k6/utils.js
@@ -0,0 +1,74 @@
+import { check } from 'k6';
+
+export let checks = {
+
+	status: function checkStatus(name, response, expectedStatus) {
+		let checks = {};
+		checks[`${name} / response status is ${expectedStatus}`] = 
+			r => expectedStatus == r.status;
+		check(response, checks);
+	},
+
+	value: function checkValue(name, fieldName, expected, toCheck) {
+		let checks = {};
+		if (no(toCheck) || toCheck == "") {
+			if (no(expected) || expected == "") {
+				toCheck = "";
+				expected = "";
+			}
+		}
+		checks[`${name} / value of field ${fieldName}`] = 
+			value => {
+				if (expected == value) return true;
+				console.log(`WARNING: Check for field ${name}/${__ITER}_${__VU}/'${fieldName}' failed:\nExpected <${expected}>\n     Got <${value}>\nEnd of WARNING`);
+				return false;
+			};
+		check(toCheck, checks);
+	},
+};
+
+export function getCities(cityList, countryCode) {
+	let cities = [];
+	cityList.forEach(city => {
+		if (city.country == countryCode) {
+			cities.push(city.name);
+		}
+	});
+	return cities;
+}
+
+export function defaultOrEnv(defaultValue, envName, trace = true) {
+	let value = __ENV[envName];
+	let isDefault = false;
+	if (no(value)) {
+		value = defaultValue;
+		isDefault = true;
+	}
+	value = convertBooleanString(value);
+	if (trace) {
+		console.log(`${envName}: ${value}, is default: ${isDefault}, type ${typeof value}`);
+	}
+	return value;
+}
+
+export function no(value) {
+	return value == null || value == undefined;
+}
+
+function convertBooleanString(value) {
+	if (typeof value != "string") {
+		return value;
+	}
+
+	switch(value.toLowerCase()) {
+	case "false":
+	case "no":
+		return false;
+	case "true":
+	case "yes":
+		return true;
+	}
+
+	return value;
+}
+
---
+++ src/test/k6/weather-test.js
@@ -0,0 +1,55 @@
+import { sleep } from 'k6';
+import http from 'k6/http';
+
+import { checks, getCities, defaultOrEnv } from './utils.js';
+
+const cityList = JSON.parse(open('./city.list.json'));
+
+export function setup() {
+	let data = {
+		server: "localhost",
+		port: 8080,
+		path: "weather",
+		cityNames: null,
+		countVus: parseInt(defaultOrEnv(0, "K6_VUS")),
+		index: 0,
+	};
+	if (data.countVus == 0) {
+		console.log("ERROR: Please specify the exact number of VUs in the 'K6_VUS' variable!");
+		return data;
+	}
+	data.cityNames = getCities(cityList, "US");
+	//console.log(data.cityNames);
+	console.log(`Endpoint ${data.server}:${data.port}/${data.path}, available cities count ${data.cityNames.length}`);
+	return data;
+}
+
+export default function weather(data) {
+	if (__VU > data.countVus) {
+		console.log(`VU ${__VU} > total VUs ${data.countVus}, doing nothing`);
+		return;
+	}
+
+	if (data.index == 0) {
+		for (let i = 0; i < __VU - 1; i++) {
+			data.index++;
+		}
+	}
+	let city = data.cityNames[data.index];
+	//console.log(`VU ${__VU}, index ${data.index}, city ${city}`);
+
+	let url = `http://${data.server}:${data.port}/${data.path}?city=${encodeURIComponent(city)}`;
+	let expectedStatus = 200;
+	let r = http.post(url);
+	if (r.status != expectedStatus) {
+		console.log(r.body);
+	}
+	checks.status("weather", r, expectedStatus);
+	let d = JSON.parse(r.body);
+	checks.value("weather", "city", city, d.city);
+
+	data.index = data.index + data.countVus;
+}
+
+export function teardown(data) {
+}
